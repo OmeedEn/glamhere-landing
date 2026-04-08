2b4039b4497a73ac63929e764eecbba20f5d35e9
Omeed Enshaie <enshaieomeed@gmail.com>
2026-04-08 08:45:20 -0700

    Update App Store URL to the correct link for GlamHere app

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,4 +1,4 @@
 // TODO: Replace these with your actual App Store and Google Play URLs
-export const APP_STORE_URL = "https://apps.apple.com/app/glamhere/id0000000000";
+export const APP_STORE_URL = "https://apps.apple.com/us/app/glamhere/id6748452704";
 export const GOOGLE_PLAY_URL =
   "https://play.google.com/store/apps/details?id=com.glamhere";
